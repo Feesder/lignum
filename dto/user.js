f205a64bc8e9d1bde620e2f7cc8e8066bcc43d3a
Feesder <qop6261@gmail.com>
2023-07-21 20:59:07 +0600

    change dto

--- dto/user.js
+++ dto/user.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const User = new mongoose.Schema({
     discord_id: {type: String, required: true, unique: true},
-    minecraft_id: {type: String, required: false,  unique: true},
+    minecraft_id: {type: String, required: false,  unique: false},
     username: {type: String, required: true},
     registered: {type: Date, default: Date.now(), required: true}
 })
